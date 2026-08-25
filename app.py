5c34cbe1e1c2d67c9df9d7ba8c4e2016ca4f02fb
Kittyboy06 <afsalahmed2006@gmail.com>
2026-08-25 12:04:38 +0530

    feat: initialize Flask app and add YouTube search query helper function

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import os, re, urllib.parse, urllib.request
 from flask import Flask, abort, render_template, request, jsonify
+
+app = Flask(__name__)
+
+def get_vid(q):
+    try:
+        enc = urllib.parse.quote(q)
+        url = f"https://www.youtube.com/results?search_query={enc}"
+    except:
+        return None, "Error Fetching Video"
